4557de9d8f33385d4fe184d79483ac3eccf44413
alan <alanarostegui@hotmail.com>
2020-05-14 20:27:43 -0500

    rest server(server): peticiones

--- cafe/server/server.js
+++ cafe/server/server.js
@@ -1,12 +1,37 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const PORT = 3000;
 
-app.get('/', function(req,res) {
-    res.json('Hello world')
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json())
+
+app.get("/", function (req, res) {
+  res.json("Hello world");
+});
+
+app.get("/usuario", function (req, res) {
+  res.json("get usuario");
+});
+
+app.post("/usuario", function (req, res) {
+    let body = req.body;
+  res.json({
+      body
+  });
+});
+
+app.put("/usuario/:id", function (req, res) {
+  let id = req.params.id;
+  res.json({
+      id
+  });
+});
+
+app.delete("/usuario", function (req, res) {
+  res.json("delete usuario");
 });
 
 app.listen(PORT, () => {
-    console.log(`Escuchando en el puerto: ${PORT}`)
-})
+  console.log(`Escuchando en el puerto: ${PORT}`);
+});
