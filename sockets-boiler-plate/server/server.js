fd965bd1156f7cd7ffecbbe1c8ecd95a2d775cac
alan <alanarostegui@hotmail.com>
2020-05-16 17:28:38 -0500

    socket-boiler-plate(conect user): conect user

--- sockets-boiler-plate/server/server.js
+++ sockets-boiler-plate/server/server.js
@@ -1,24 +1,26 @@
-const express = require('express');
-const socketIO = require('socket.io');
-const http = require('http');
+const express = require("express");
+const socketIO = require("socket.io");
+const http = require("http");
 
-const path = require('path');
+const path = require("path");
 
 const app = express();
 let server = http.createServer(app);
 
-const publicPath = path.resolve(__dirname, '../public');
+const publicPath = path.resolve(__dirname, "../public");
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
 //IO = Comunicación de sockets del backend
-let io = socketIO(server)
+let io = socketIO(server);
+
+io.on("connection", (client) => {
+  console.log("Usuario conectado");
+});
 
 server.listen(port, (err) => {
+  if (err) throw new Error(err);
 
-    if (err) throw new Error(err);
-
-    console.log(`Servidor corriendo en puerto ${ port }`);
-
+  console.log(`Servidor corriendo en puerto ${port}`);
 });
