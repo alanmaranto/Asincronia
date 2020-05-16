c07624679bbd5b6c565456307e80bb4aa9801e8c
alan <alanarostegui@hotmail.com>
2020-05-16 17:48:45 -0500

    socket-boiler-plate(): emit from server and listenning on cliente

--- sockets-boiler-plate/server/server.js
+++ sockets-boiler-plate/server/server.js
@@ -18,6 +18,11 @@ let io = socketIO(server);
 io.on("connection", (client) => {
   console.log("Usuario conectado");
 
+  client.emit("sendMessage", {
+    user: "Admin",
+    message: "Welcome to this app",
+  });
+
   client.on("disconnect", () => {
     console.log("Usuario desconectado");
   });
