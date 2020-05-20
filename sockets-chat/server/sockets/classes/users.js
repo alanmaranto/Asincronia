92b1ae33e7888741dcee34883823ac1547bd4756
alan <alanarostegui@hotmail.com>
2020-05-19 20:48:16 -0500

    socket-chat(user class): create all the functions

--- sockets-chat/server/sockets/classes/users.js
+++ sockets-chat/server/sockets/classes/users.js
@@ -1,7 +1,42 @@
 class Users {
+  constructor() {
+    this.users = [];
+  }
 
+  addUser(id, name) {
+    let user = {
+      id,
+      name,
+    };
+
+    this.users.push(user);
+
+    return this.users;
+  }
+
+  getUser(id) {
+    let user = this.users.filter((user) => user.id === id)[0]; // The first element who coincides with the id of the array w
+
+    return user;
+  }
+
+  getAllUsers() {
+    return this.users;
+  }
+
+  getUsersByRoom(room) {
+    //
+  }
+
+  deleteUser(id) {
+    let deletedUser = this.getUser(id);
+    // Replace the array of users with the users active in the chat
+    this.users = this.users.filter((user) => user.id != id);
+
+    return deletedUser;
+  }
 }
 
 module.exports = {
-    Users
-}
+  Users,
+};
