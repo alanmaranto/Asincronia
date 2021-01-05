e717b686bc4b199f7fb395d3d16a288f248b3e20
alan <alanarostegui@hotmail.com>
2021-01-05 16:32:56 -0600

    TasksToDo: fileSystem and requires

--- 05 - Task To Do/app.js
+++ 05 - Task To Do/app.js
@@ -1,5 +1,16 @@
-let base = 3;
+const fs = require("fs");
+// const fs = require('express');
+// const fs = require('../');
+
+let base = 6;
+let data = '';
 
 for (let i = 1; i <= 10; i++) {
-  console.log(`${base} * ${i} = ${base * i}`);
+  data += `${base} * ${i} = ${base * i}\n`;
 }
+
+fs.writeFile(`tablas/tabla${base}.txt`, data, (err) => {
+  if (err) throw err;
+
+  console.log("El archivo ha sido creado");
+});
